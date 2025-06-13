f40aad105dc8bea8b072cb9377e024810ba2867a
张欢 <570436048@qq.com>
2025-06-13 22:03:30 +0800

    引入eslint插件

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,15 @@ import './index.css';
 // import './wdyr';
 import { createRoot } from 'react-dom/client';
 
-const App = ()=>{
-    return (
-        <>
-        <h1>我的web3-DAPP1213132</h1>
-        <span className='titleName'>hhahahhah</span>
-        <span className='text-3xl font-bold underline text-cyan-500'>hello world</span>
-        </>
-    )
-}
+const App = () => {
+  return (
+    <>
+      <h1>我的web3-DAPP1213132</h1>
+      <span className="titleName">hhahahhah</span>
+      <span className="text-3xl font-bold underline text-cyan-500">hello world</span>
+    </>
+  );
+};
 
 const container = document.getElementById('app');
 if (!container) {
